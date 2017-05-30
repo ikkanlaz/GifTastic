63f395c319ac1f14c0b7e987f60cfab588bf1ee4
ikkanlaz <andrew.lazenby@gmail.com>
2017-05-29 20:28:11 -0700

    adding rough draft

---
+++ assets/js/app.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+    var reactionList = ["happy", "excited", "sad", "love", "thumbs up", "shrug", "surprised", "awkward", "angry", "confused"];
+    var pageNum = 0;
+
+    function renderButtons() {
+        $(".btn-container").empty();
+        for (var i = 0; i < reactionList.length; i++) {
+            var newBtn = $("<button></button>");
+            newBtn.addClass("btn btn--reaction");
+            newBtn.text(reactionList[i]);
+            newBtn.attr("value", reactionList[i]);
+            $(".btn-container").append(newBtn);
+        }
+    }
+
+    $(".reaction-submit").on("click", function (event) {
+        event.preventDefault();
+        var newReaction = $("#reaction-input").val().trim();
+        if (newReaction.length !== 0) {
+            reactionList.push(newReaction);
+            renderButtons();
+        }
+    });
+
+    $(document).on("click", ".btn--reaction", function () {
+        $(".page-container").empty();
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + this.value + "&offset=" + pageNum + "&limit=24&api_key=dc6zaTOxFJmzC";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+            if (response.meta.status === 200) {
+                var numGifs = response.pagination.count;
+                var newGifBlock = $("<div></div>");
+                newGifBlock.addClass("gif-page");
+                for (var i = 0; i < numGifs; i++) {
+                    var newGifContainer = $("<div></div>");
+                    newGifContainer.addClass("gif-container");
+                    var newGif = $("<img>");
+                    newGif.addClass("gif");
+                    console.log(response.data[i].images.fixed_width_still.url);
+                    newGif.attr("src", response.data[i].images.fixed_width_still.url);
+                    newGif.attr("data-gif", response.data[i].images.fixed_width.url)
+                    newGif.attr("data-still", response.data[i].images.fixed_width_still.url)
+                    newGifContainer.append(newGif);
+
+                    var newGifRating = $("<span></span>");
+                    newGifRating.addClass("gif-rating");
+                    newGifRating.text(response.data[i].rating.toUpperCase());
+                    newGifContainer.append(newGifRating);
+
+                    newGifBlock.append(newGifContainer);
+                }
+                $(".page-container").append(newGifBlock);
+            }
+        });
+    });
+    $(document).on("click", ".gif", function () {
+        var url = $(this).attr("src");
+        if (url !== $(this).attr("data-gif")) {
+            $(this).attr("src", $(this).attr("data-gif"));
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+        }
+    });
+
+    renderButtons();
+
+
+});
